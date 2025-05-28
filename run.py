e34b329137fd82bc40982b62764f93d4430046f9
VanillaMile <94708909+VanillaMile@users.noreply.github.com>
2025-05-28 13:59:31 +0200

    Saving/Loading game state from a file

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 from game import GameEngine, Player
+import pickle
 
 if __name__ == "__main__":
-    player1 = Player(100, 'Satori')
-    player2 = Player(100, 'Koishi')
+    _input = input("Load a game? (y/n)[n]: ")
+    if _input.lower() == 'y':
+        game = pickle.load(open('game_save.pkl', 'rb'))
+    else:
+        player1 = Player(1000, 'Satori')
+        player2 = Player(1000, 'Koishi')
 
-    game = GameEngine([player1, player2], start_card=7)
+        game = GameEngine([player1, player2], start_card=7)
+
+    
     game.play_round()
+
+    _input = input("Save the game? (y/n)[n]: ")
+    if _input.lower() == 'y':
+        with open('game_save.pkl', 'wb') as f:
+            pickle.dump(game, f)
+    else:
+        print("Game not saved.")
